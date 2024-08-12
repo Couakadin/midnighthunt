520ed95c3db9bb1a2de18c55af27acb07f76c55a
Valentin <webdev.valentin@gmail.com>
2024-08-12 16:14:28 +0200

    feat: player fps movement

--- Assets/Project/Features/MidnightHunt/Player/PlayerController.cs
+++ Assets/Project/Features/MidnightHunt/Player/PlayerController.cs
@@ -1,7 +1,6 @@
 using Data.Runtime;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Midnighthunt.Runtime
 {
@@ -15,21 +14,42 @@ namespace Midnighthunt.Runtime
 
         private void Awake()
         {
+            _inputController = new InputController();
+
+            _camera = Camera.main;
+
+            Cursor.visible = false;
+
             if (_playerBlackboard.ContainsKey("PlayerSpeed"))
                 _playerSpeed = _playerBlackboard.GetValue<float>("PlayerSpeed");
+            if (_playerBlackboard.ContainsKey("JumpHeight"))
+                _jumpHeight = _playerBlackboard.GetValue<float>("JumpHeight");
+            if (_playerBlackboard.ContainsKey("GravityValue"))
+                _gravityValue = _playerBlackboard.GetValue<float>("GravityValue");
+            if (_playerBlackboard.ContainsKey("GroundedPlayer"))
+                _isGroundedPlayer = _playerBlackboard.GetValue<bool>("GroundedPlayer");
         }
 
         private void FixedUpdate()
         {
-            Vector3 velocity = _moveInput * _playerSpeed;
-            _playerRigidbody.velocity = velocity;
+            Vector3 velocity = _camera.transform.forward * GetPlayerMovement().z + _camera.transform.right * GetPlayerMovement().x;
+            _playerRigidbody.velocity = velocity * _playerSpeed;
+            _playerRigidbody.velocity = new Vector3(_playerRigidbody.velocity.x, 0f, _playerRigidbody.velocity.z);
         }
+
+        private void OnEnable() => _inputController.Enable();
+
+        private void OnDisable() => _inputController.Disable();
 
         #endregion
 
         #region Methods
 
-        public void OnMoveInput(InputAction.CallbackContext context) => _moveInput = context.ReadValue<Vector3>();
+        public Vector3 GetPlayerMovement() => _inputController.Player.Move.ReadValue<Vector3>();
+        
+        public Vector3 GetMouseDelta() => _inputController.Player.Look.ReadValue<Vector3>();
+
+        public bool IsJumping() => _inputController.Player.Jump.triggered;
 
         #endregion
 
@@ -44,11 +64,19 @@ namespace Midnighthunt.Runtime
         private Blackboard _playerBlackboard;
         [SerializeField]
         private Rigidbody _playerRigidbody;
-        
+        [SerializeField]
+        private CharacterController _characterController;
+
+        private InputController _inputController;
+
+        private Camera _camera;
+
+        private float _jumpHeight;
         private float _playerSpeed;
+        private float _gravityValue;
 
-        private Vector3 _moveInput;
-
+        private bool _isGroundedPlayer;
+ 
         #endregion
     }
 }
